446b240b4e7c4a48dc9d8c918101902ab049fcf4
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-25 23:52:37 -0200

    Aula 4.3: Otimizando mapas com tabelas de espalhamento

--- tw-estrutura-dados/src/br/com/treinaweb/estruturadados/mapas/Mapa.java
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/mapas/Mapa.java
@@ -1,5 +1,21 @@
 package br.com.treinaweb.estruturadados.mapas;
 
+import br.com.treinaweb.estruturadados.listasligadas.ListaLigada;
+
 public class Mapa<K, V> {
+
+	private ListaLigada<ListaLigada<Associacao<K, V>>> elementos;
+	private int quantidadeCategorias = 16;
+
+	public Mapa() {
+		this.elementos = new ListaLigada<ListaLigada<Associacao<K, V>>>();
+		for (int i = 0; i < this.quantidadeCategorias; i++) {
+			this.elementos.inserir(new ListaLigada<Associacao<K, V>>());
+		}
+	}
+
+	private int gerarNumeroEspalhamento(K chave) {
+		return Math.abs(chave.hashCode() % this.quantidadeCategorias);
+	}
 
 }
