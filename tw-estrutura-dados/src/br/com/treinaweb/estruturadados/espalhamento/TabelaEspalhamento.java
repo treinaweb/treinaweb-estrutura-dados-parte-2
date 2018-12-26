0a418033f7761e0c9a71071ed57bb162ccfb7f93
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-25 23:25:11 -0200

    Aula 3.4: Implementando as funcionalidades de remoção e de verificação de existência de elementos em uma tabela de espalhamento

--- tw-estrutura-dados/src/br/com/treinaweb/estruturadados/espalhamento/TabelaEspalhamento.java
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/espalhamento/TabelaEspalhamento.java
@@ -10,26 +10,51 @@ public class TabelaEspalhamento<T> {
 
 	private ListaLigada<ListaLigada<T>> elementos;
 	private int numeroCategorias = 16;
+	private int tamanho;
 
 	public TabelaEspalhamento() {
 		this.elementos = new ListaLigada<ListaLigada<T>>();
 		for (int i = 0; i < this.numeroCategorias; i++) {
 			this.elementos.inserir(new ListaLigada<T>());
 		}
+		this.tamanho = 0;
 	}
 
 	public boolean inserir(T elemento) {
-		int numeroEspalhamento = this.gerarNumeroEspalhamento(elemento);
-		ListaLigada<T> categoria = this.elementos.recuperar(numeroEspalhamento);
-		if (categoria.contem(elemento)) {
+		if (this.contem(elemento)) {
 			return false;
 		}
+		int numeroEspalhamento = this.gerarNumeroEspalhamento(elemento);
+		ListaLigada<T> categoria = this.elementos.recuperar(numeroEspalhamento);
 		categoria.inserir(elemento);
+		this.tamanho++;
 		return true;
 	}
 
+	public void remover(T elemento) {
+		int numeroEspalhamento = this.gerarNumeroEspalhamento(elemento);
+		ListaLigada<T> categoria = this.elementos.recuperar(numeroEspalhamento);
+		categoria.remover(elemento);
+		this.tamanho--;
+	}
+
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	public boolean contem(T elemento) {
+		int numeroEspalhamento = this.gerarNumeroEspalhamento(elemento);
+		ListaLigada<T> categoria = this.elementos.recuperar(numeroEspalhamento);
+		return categoria.contem(elemento);
+	}
+
 	private int gerarNumeroEspalhamento(T elemento) {
-		return elemento.hashCode() % 16;
+		return elemento.hashCode() % this.numeroCategorias;
+	}
+
+	@Override
+	public String toString() {
+		return "TabelaEspalhamento [elementos=" + elementos + "]";
 	}
 
 }
