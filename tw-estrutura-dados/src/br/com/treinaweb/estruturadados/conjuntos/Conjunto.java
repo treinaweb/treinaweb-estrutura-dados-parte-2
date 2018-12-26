b1ce9e8a06754667897e158e0e532dc9bccfd0e0
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-25 22:59:00 -0200

    Aula 2.2 - Definindo o que é um set (ou conjunto)

---
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/conjuntos/Conjunto.java
@@ -0,0 +1,41 @@
+package br.com.treinaweb.estruturadados.conjuntos;
+
+public class Conjunto<T> {
+
+	public void inserir(T elemento) {
+
+	}
+
+	public void inserirEm(int posicao, T elemento) {
+
+	}
+
+	public T recuperar(int posicao) {
+		return null;
+	}
+
+	public boolean estaVazio() {
+		return false;
+	}
+
+	public int tamanho() {
+		return 0;
+	}
+
+	public boolean contem(T elemento) {
+		return false;
+	}
+
+	public int indice(T elemento) {
+		return 0;
+	}
+
+	public void remover(int posicao) {
+
+	}
+
+	public void remover(T elemento) {
+
+	}
+
+}
