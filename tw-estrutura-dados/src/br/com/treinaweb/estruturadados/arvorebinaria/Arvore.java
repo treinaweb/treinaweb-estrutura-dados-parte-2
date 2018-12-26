7c60403daeaf7b7154bbe6e4584ebc7bd926ffa4
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-26 00:07:56 -0200

    Aula 5.3: Implementando algoritmos recursivos no processo de inserção de nós em uma árvore binária

--- tw-estrutura-dados/src/br/com/treinaweb/estruturadados/arvorebinaria/Arvore.java
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/arvorebinaria/Arvore.java
@@ -1,24 +1,40 @@
 package br.com.treinaweb.estruturadados.arvorebinaria;
 
 public class Arvore<T> {
-	
+
 	private NoArvore<T> raiz;
-	
+
 	public Arvore() {
 		this.raiz = null;
 	}
-	
-	public NoArvore<T> getRaiz(){
+
+	public NoArvore<T> getRaiz() {
 		return this.raiz;
 	}
-	
+
 	public void inserir(NoArvore<T> no) {
 		no.setNoDireito(null);
 		no.setNoEsquerdo(null);
 		if (this.raiz == null) {
 			this.raiz = no;
-		} else if (raiz.peso() < no.peso()) {
-			this.raiz.setNoDireito(no);
+		} else {
+			this.inserir(this.raiz, no);
+		}
+	}
+
+	private void inserir(NoArvore<T> ref, NoArvore<T> novoNo) {
+		if (ref.peso() < novoNo.peso()) {
+			if (ref.getNoDireito() == null) {
+				ref.setNoDireito(novoNo);
+			} else {
+				inserir(ref.getNoDireito(), novoNo);
+			}
+		} else {
+			if (ref.getNoEsquerdo() == null) {
+				ref.setNoEsquerdo(novoNo);
+			} else {
+				inserir(ref.getNoEsquerdo(), novoNo);
+			}
 		}
 	}
 
