6d6327f8cd350f60f519dd7f7d02c361c5f02c7d
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-25 23:54:14 -0200

    Aula 4.4: Implementando a pesquisa por chaves e remoção de elementos

--- tw-estrutura-dados/src/br/com/treinaweb/estruturadados/mapas/Mapa.java
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/mapas/Mapa.java
@@ -14,6 +14,30 @@ public class Mapa<K, V> {
 		}
 	}
 
+	public boolean contemChave(K chave) {
+		int numeroEspalhamento = this.gerarNumeroEspalhamento(chave);
+		ListaLigada<Associacao<K, V>> categoria = this.elementos.recuperar(numeroEspalhamento);
+		for (int i = 0; i < categoria.tamanho(); i++) {
+			Associacao<K, V> associacao = categoria.recuperar(i);
+			if (associacao.getChave().equals(chave)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void remover(K chave) {
+		int numeroEspalhamento = this.gerarNumeroEspalhamento(chave);
+		ListaLigada<Associacao<K, V>> categoria = this.elementos.recuperar(numeroEspalhamento);
+		for (int i = 0; i < categoria.tamanho(); i++) {
+			Associacao<K, V> associacao = categoria.recuperar(i);
+			if (associacao.getChave().equals(chave)) {
+				categoria.remover(associacao);
+				return;
+			}
+		}
+	}
+
 	private int gerarNumeroEspalhamento(K chave) {
 		return Math.abs(chave.hashCode() % this.quantidadeCategorias);
 	}
