f79e0c4997f9a0f11fc1cd2f1468a704264e8b2f
Cleber Campomori <cleber.campomori@gmail.com>
2018-12-25 23:05:03 -0200

    Aula 2.3: iniciando a implementação de um set ou conjunto

--- tw-estrutura-dados/src/br/com/treinaweb/estruturadados/conjuntos/Conjunto.java
+++ tw-estrutura-dados/src/br/com/treinaweb/estruturadados/conjuntos/Conjunto.java
@@ -1,41 +1,49 @@
 package br.com.treinaweb.estruturadados.conjuntos;
 
+import br.com.treinaweb.estruturadados.listasligadas.ListaLigada;
+
 public class Conjunto<T> {
 
-	public void inserir(T elemento) {
+	private ListaLigada<T> elementos;
 
+	public Conjunto() {
+		this.elementos = new ListaLigada<T>();
+	}
+
+	public void inserir(T elemento) {
+		this.elementos.inserir(elemento);
 	}
 
 	public void inserirEm(int posicao, T elemento) {
-
+		this.elementos.inserirEm(posicao, elemento);
 	}
 
 	public T recuperar(int posicao) {
-		return null;
+		return this.elementos.recuperar(posicao);
 	}
 
 	public boolean estaVazio() {
-		return false;
+		return this.elementos.estaVazia();
 	}
 
 	public int tamanho() {
-		return 0;
+		return this.elementos.tamanho();
 	}
 
 	public boolean contem(T elemento) {
-		return false;
+		return this.elementos.contem(elemento);
 	}
 
 	public int indice(T elemento) {
-		return 0;
+		return this.elementos.indice(elemento);
 	}
 
 	public void remover(int posicao) {
-
+		this.elementos.remover(posicao);
 	}
 
 	public void remover(T elemento) {
-
+		this.elementos.remover(elemento);
 	}
 
 }
